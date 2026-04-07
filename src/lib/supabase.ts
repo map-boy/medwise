2a885cd59da0d55fce2b860a8ba19fe487f20cfe
map-boy <umwemubi@gmail.com>
2026-04-07 16:10:20 +0200

    fix: supabase init for vercel build

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,3 +1,4 @@
+$originalCode = @"
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
@@ -6,3 +7,5 @@ const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: { persistSession: true, autoRefreshToken: true },
 });
+"@
+[System.IO.File]::WriteAllText("$(Get-Location)\src\lib\supabase.ts", $originalCode, [System.Text.Encoding]::UTF8)
